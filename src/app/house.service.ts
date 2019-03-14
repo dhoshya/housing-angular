d9b792460fa734188db78ccf1384964f65c313ce
Rohan Joshi <joshi.rohan09@gmail.com>
2019-03-14 18:16:09 -0400

    fixed footer, adjusted navbar

--- src/app/house.service.ts
+++ src/app/house.service.ts
@@ -21,7 +21,7 @@ export class HouseService {
         distance: '1 Mile', shuttleservice: true, flatmattes: 5
       },
         { id: ' q0003', imageUrl: '../../../assets/images/avalon.jpg',
-        username: 'Simaran Rajora', rental: 320, community: 'Cavilier Court', beds: 3, baths: 2, rentalType: 'girls', veggie: true,
+        username: 'Sneha', rental: 320, community: 'Gainsborough', beds: 3, baths: 2, rentalType: 'girls', veggie: true,
         // tslint:disable-next-line:max-line-length
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         contactno: '+1-571-778-8434', dryer: true, aircontrol: true, garage: true, nearby: 'Walmart, Giant',
@@ -32,19 +32,19 @@ export class HouseService {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         contactno: '+1-571-778-8434', dryer: true, aircontrol: true, garage: true, nearby: 'GMU, University Mall, Main Street',
         distance: '1 Mile', shuttleservice: true, flatmattes: 5},
-        { id: ' q0004', imageUrl: '../../../assets/images/avalon.jpg',
-        username: 'Aniket Bhosale', rental: 360, community: 'Circle Tower', beds: 3, baths: 2, rentalType: 'boys', veggie: false,
+        { id: ' q0005', imageUrl: '../../../assets/images/avalon.jpg',
+        username: 'Akshay Rane ', rental: 500, community: 'Central Circle', beds: 3, baths: 2, rentalType: 'boys', veggie: false,
         // tslint:disable-next-line:max-line-length
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         contactno: '+1-571-778-8434', dryer: true, aircontrol: true, garage: true, nearby: 'GMU, University Mall, Main Street',
         distance: '1 Mile', shuttleservice: true, flatmattes: 5},
-        { id: ' q0004', imageUrl: '../../../assets/images/avalon.jpg',
-        username: 'Aniket Bhosale', rental: 360, community: 'Circle Tower', beds: 3, baths: 2, rentalType: 'boys', veggie: false,
+        { id: ' q0006', imageUrl: '../../../assets/images/avalon.jpg',
+        username: 'Brinston Gonsalves', rental: 700, community: 'Ascent', beds: 3, baths: 2, rentalType: 'boys', veggie: false,
         // tslint:disable-next-line:max-line-length
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         contactno: '+1-571-778-8434', dryer: true, aircontrol: true, garage: true, nearby: 'GMU, University Mall, Main Street',
         distance: '1 Mile', shuttleservice: true, flatmattes: 5}
-        
+
       ];
 
     getHouses() {
