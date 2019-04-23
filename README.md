1b14ea681e9522f0ad23898096895d9475cdbfd5
Rohan Joshi <joshi.rohan09@gmail.com>
2019-04-23 15:12:46 -0400

    minor service changes, readme updated.

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Housing
 
-Housing Web App where people can find apartments near by George Mason University.
-The web-app is created using Angular JS and Spring Boot.
+Housing Web App is a place where people can find apartments near George Mason University.
+The web-app is created using Angular 2+ using node.js/ExpressJS with MongoDB.
 
-Forking from jpalan23 to add the listings pages and work on backend using Spring.
+This app is being developed by two George Mason graduates, who served on the
+board of Indian Graduate Student's Association, and took upon to solve a commonly
+faced problem by the community, not only ours but the student community.
+
+This product has a great marketable potential across GMU as well as
+other universities because students are always looking for places to live near
+the University.
+
+Forking from jpalan23 to add the listings pages and work on backend using
+node.js/ExpressJS and set up database with MongoDB.
--- src/app/house.service.ts
+++ src/app/house.service.ts
@@ -13,32 +13,36 @@ export class HouseService {
         baths: 2, rentalType: 'mix', veggie: true, description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         contactno: '+1-571-778-8434', dryer: true, aircontrol: true, garage: false, nearby: 'GMU, University Mall, Main Street',
         distance: '1 Mile', shuttleservice: true, flatmattes: 5},
-        { id: ' q0002', imageUrl: '../../../assets/images/avalon.jpg',
+
+        { id: 'q0002', imageUrl: '../../../assets/images/avalon.jpg',
         username: 'Rohan Joshi', rental: 340, community: 'Circle Tower', beds: 3, baths: 2, rentalType: 'boys', veggie: false,
         // tslint:disable-next-line:max-line-length
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         contactno: '+1-571-778-8434', dryer: false, aircontrol: false, garage: false, nearby: 'Safeway, Vienna Metro Station',
-        distance: '1 Mile', shuttleservice: true, flatmattes: 5
-      },
-        { id: ' q0003', imageUrl: '../../../assets/images/avalon.jpg',
+        distance: '1 Mile', shuttleservice: true, flatmattes: 5},
+
+        { id: 'q0003', imageUrl: '../../../assets/images/avalon.jpg',
         username: 'Sneha', rental: 320, community: 'Gainsborough', beds: 3, baths: 2, rentalType: 'girls', veggie: true,
         // tslint:disable-next-line:max-line-length
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         contactno: '+1-571-778-8434', dryer: true, aircontrol: true, garage: true, nearby: 'Walmart, Giant',
         distance: '1 Mile', shuttleservice: true, flatmattes: 5},
-        { id: ' q0004', imageUrl: '../../../assets/images/avalon.jpg',
+
+        { id: 'q0004', imageUrl: '../../../assets/images/avalon.jpg',
         username: 'Aniket Bhosale', rental: 360, community: 'Circle Tower', beds: 3, baths: 2, rentalType: 'boys', veggie: false,
         // tslint:disable-next-line:max-line-length
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         contactno: '+1-571-778-8434', dryer: true, aircontrol: true, garage: true, nearby: 'GMU, University Mall, Main Street',
         distance: '1 Mile', shuttleservice: true, flatmattes: 5},
-        { id: ' q0005', imageUrl: '../../../assets/images/avalon.jpg',
+
+        { id: 'q0005', imageUrl: '../../../assets/images/avalon.jpg',
         username: 'Akshay Rane ', rental: 500, community: 'Central Circle', beds: 3, baths: 2, rentalType: 'boys', veggie: false,
         // tslint:disable-next-line:max-line-length
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         contactno: '+1-571-778-8434', dryer: true, aircontrol: true, garage: true, nearby: 'GMU, University Mall, Main Street',
         distance: '1 Mile', shuttleservice: true, flatmattes: 5},
-        { id: ' q0006', imageUrl: '../../../assets/images/avalon.jpg',
+
+        { id: 'q0006', imageUrl: '../../../assets/images/avalon.jpg',
         username: 'Brinston Gonsalves', rental: 700, community: 'Ascent', beds: 3, baths: 2, rentalType: 'boys', veggie: false,
         // tslint:disable-next-line:max-line-length
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
